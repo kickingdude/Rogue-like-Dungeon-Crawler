7464a0fbaf629bb1d75698260aa50090e12f2255
Kyla Do <kd.katiedo3@gmail.com>
2024-08-23 19:10:17 -0700

    main menu

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -36,7 +36,10 @@ public class Enemy : MonoBehaviour
     // have enemies move towards the player
     private void Swarm()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        if(player != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
